d68736e5252524e00d714b8b76f06f7cc83ad178
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-02-21 06:31:24 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,168 @@
+# name = 'adnan'
+# name = 'shafiq'
+# productPrice = 32.23
+# isOnline = True
+# print(name)
+# print(productPrice)
+# print(isOnline)
+
+#########################################
+
+# patientName = 'John Smith'
+# patientAge = 20
+# isNew = True
+# print(patientName, patientAge, isNew)
+
+#############################################
+
+# name = input('What is your name? ')
+# age = input('What is your age? ')
+# weight = input('What is your weight? ')
+
+# print(name, age, weight)
+
+###############################################
+
+# age = 2024 - int(input("What is your birth year? "))
+# print(age)
+
+############################################
+
+# firstNum = input("First Number: ")
+# secondNum = input("Second Number: ")
+# sum = float(firstNum) + float(secondNum)
+# print("Sum is " + str(sum))
+
+############################################
+
+# text = "A quick brown fox jumps over the lazy dog"
+# print(text.upper())
+# print(text.lower())
+# print(text.find('fox'))
+# print(text.find('FOX'))
+# print(text.replace('fox', 'FOX'))
+# print(text.replace('rat', 'cat'))
+# print('fox' in text)
+
+#############################################
+
+# num1 = 4
+# num2 = 3
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+# num1 += 10
+# print(num1)
+
+
+#########################################
+
+# x = 3 > 2
+# x = 3 >= 2
+# x = 3 <= 2
+# x = 3 < 2
+# x = 3 == 2
+# x = 3 != 2
+# print(x)
+
+#########################################
+
+# price = 25
+# print(10 < price < 30)
+# print(price > 10 and price < 30)
+# print(price > 10 or price < 5)
+# print(not price > 50)
+
+################################################
+
+# temp = 15
+
+# if temp >= 30:
+#     print('Hot Damn!')
+# elif temp >= 20:
+#     print('Nice Day!')
+# else:
+#     print('IceMan')
+# print('Done')
+
+##############################################
+
+# weight = float(input("Weight: "))
+# scale = input("(K)g or (L)bs: ").lower()
+# kg = 2.2
+
+# if scale == 'k':
+#     print("Weight in lbs: " + str(weight * kg))
+# elif scale == 'l':
+#     print("Weight in Kg: " + str(weight / kg))
+# else:
+#     print('Invalid Entry')
+
+####################################################
+    
+# i = 1
+# while i <= 5:
+#     print(i * '*')
+#     i = i + 1
+
+##############################################
+    
+# names = ['Adnan', 'Shafiq', 'Mosh']
+# print(names)
+# print(names[1])
+# print(names[-1])
+
+# names[2] = 'Josh'
+# print(names)
+# print(names[0:2])
+
+#########################################
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(5 in numbers)
+# print(len(numbers))
+# numbers.append(10)
+# print(numbers)
+# numbers.insert(0, 0)
+# print(numbers)
+# numbers.remove(9)
+# print(numbers)
+# numbers.clear()
+# print(numbers)
+
+#########################################
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for i in numbers:
+#     print(i)
+
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i = i+ 1
+
+###########################################
+
+# numbers = range(5)
+# for i in numbers:
+#     print(i)
+
+# numbers2 = range(4, 20)
+# for i in numbers2:
+#     print(i)
+
+# for i in range(4, 20, 5):
+#     print(i)
+
+#########################################
+
+# numbers = (1, 2, 3, 3) #immutable/tuple
+# print(numbers.index(2))
+# print(numbers.count(3))
+
+#######################################
+
